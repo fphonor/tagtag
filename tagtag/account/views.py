9f5526bb01c1ae56c07d079018813c8db045d1e2
pyskell <wangxulin108@gmail.com>
2018-01-12 15:50:54 +0800

    Add status for login failure

--- tagtag/account/views.py
+++ tagtag/account/views.py
@@ -1,30 +1,30 @@
-import inspect
-
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
 
 def index(req):
-    return HttpResponse(inspect.stack()[0][3])
+    return redirect('account_login')
 
 
 def login_(req):
     username = req.POST.get('username')
     password = req.POST.get('password')
 
+    status = 200
     context = {}
 
     if username and password:
         user = authenticate(username=username, password=password)
         if user is None:
             context = {'authenticationFailed': True}
+            status = 400
         elif not user.is_active:
             context = {'accountDisabled': True}
+            status = 403
         else:
             login(req, user)
             return render(req, "account/success.html", context)
-    return render(req, "account/login.html", context)
+    return render(req, "account/login.html", context, status=status)
 
 
 def logout_(req):
