b11c91f8ddb63b2e31da45e9050b3aef42305578
pyskell <wangxulin108@gmail.com>
2018-02-05 14:29:38 +0800

    Add `material.schema` to global settings

--- tagtag/schema.py
+++ tagtag/schema.py
@@ -3,12 +3,14 @@ import graphene
 import links.schema
 import account.schema
 import label.schema
+import material.schema
 
 
 class Query(
         account.schema.Query,
         links.schema.Query,
         label.schema.Query,
+        material.schema.Query,
         graphene.ObjectType,
 ):
     pass
@@ -18,6 +20,7 @@ class Mutation(
         account.schema.Mutation,
         links.schema.Mutation,
         label.schema.Mutation,
+        material.schema.Mutation,
         graphene.ObjectType,
 ):
     pass
