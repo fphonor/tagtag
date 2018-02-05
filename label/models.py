67d944c4cf5935002616e8c43a6d0af16cac1edf
pyskell <wangxulin108@gmail.com>
2018-02-05 14:25:57 +0800

    Add `label.models.Courses` and some mutate func to `Label`

--- label/models.py
+++ label/models.py
@@ -3,29 +3,36 @@ from django.db import models
 
 class Label(models.Model):
     SKILL_TYPE_CHOICES = (
-        ("tl", "听力"),
-        ("yd", "阅读"),
-        ("xz", "写作"),
-        ("ky", "口语"),
+        ('TL', '听力'),
+        ('YD', '阅读'),
+        ('XZ', '写作'),
+        ('KY', '口语'),
     )
-    skill_type = models.CharField(max_length=20, verbose_name="技能分类", choices=SKILL_TYPE_CHOICES)
+    skill_type = models.CharField(max_length=20, verbose_name='技能分类', choices=SKILL_TYPE_CHOICES)
 
     LABEL_TYPE_CHOICES = (
-        ("0", "微技能"),
-        ("1", "内容框架"),
+        ('WJN', '微技能'),
+        ('NRKJ', '内容框架'),
     )
-    label_type = models.CharField(max_length=10, verbose_name="标签分类", choices=LABEL_TYPE_CHOICES)
+    label_type = models.CharField(max_length=10, verbose_name='标签分类', choices=LABEL_TYPE_CHOICES)
 
-    label_level = models.IntegerField(verbose_name="标签级别")
+    label_level = models.IntegerField(verbose_name='标签级别')
 
-    label_name = models.CharField(max_length=50, verbose_name="名称")
+    label_name = models.CharField(max_length=50, verbose_name='名称')
 
     parent = models.ForeignKey(
         'label.Label',
         related_name='children',
         on_delete=models.deletion.CASCADE,
-        verbose_name="父标签ID",
+        verbose_name='父标签ID',
         null=True,
     ) # 根节点父ID默认为0
     date_joined= models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+
+
+class Course(models.Model):
+    id = models.CharField(db_column='course_id', max_length=100, verbose_name='课程ID', primary_key=True, default="")
+    name = models.CharField(db_column='course_name', max_length=100, verbose_name='课程名称', default="")
+    name_zh = models.CharField(db_column='course_ch_name', max_length=100, verbose_name='课程中文名称', default="")
+    unit_num = models.IntegerField(verbose_name='单元数量', default=10)
--- label/schema.py
+++ label/schema.py
@@ -2,12 +2,18 @@ from graphene_django import DjangoObjectType
 import graphene
 
 from label.models import Label
+from label.models import Course
 from account.schema import get_user
 
 
 class LabelType(DjangoObjectType):
     class Meta:
         model = Label
+
+
+class CourseType(DjangoObjectType):
+    class Meta:
+        model = Course
 
 
 class Query(graphene.ObjectType):
@@ -17,15 +23,31 @@ class Query(graphene.ObjectType):
         label_type=graphene.String(),
         label_level=graphene.Int(),
         label_name=graphene.String(),
+        label_name_like=graphene.String(),
         parent_id=graphene.Int(),
+    )
+    courses = graphene.List(
+        CourseType,
+        course_name_like=graphene.String(),
+        course_ch_name_like=graphene.String(),
     )
     label = graphene.Field(
         LabelType,
         id=graphene.ID()
     )
 
-    def resolve_labels(self, info, **kwargs):
-        return Label.objects.filter(**kwargs).all()
+    def resolve_labels(self, info, label_name_like=None, **kwargs):
+        if label_name_like:
+            return Label.objects.filter(label_name__contains=label_name_like)
+        return Label.objects.all()
+
+    def resolve_courses(self, info, course_name_like=None, course_ch_name_like=None, **kwargs):
+        courses = Course.objects
+        if course_name_like:
+            courses = courses.filter(course_name__contains=course_name_like)
+        if course_ch_name_like:
+            courses = courses.filter(course_ch_name__contains=course_ch_name_like)
+        return courses.all()
 
     def resolve_label(self, info, id):
         return Label.objects.get(pk=id)
@@ -59,5 +81,75 @@ class CreateLabel(graphene.Mutation):
         return CreateLabel(label=label)
 
 
+class CreateCourse(graphene.Mutation):
+    course = graphene.Field(CourseType)
+
+    class Arguments:
+        course_id = graphene.String()
+        course_name = graphene.String()
+        course_ch_name = graphene.String()
+        unit_num = graphene.Int()
+
+    def mutate(self, info, course_id, course_name, course_ch_name, unit_num,):
+        user = get_user(info) or None
+        if not user.is_active:
+            raise Exception('User: %s(%s) not active!' % (user.username, user.email))
+
+        course = Course(
+            course_id=course_id,
+            course_name=course_name,
+            course_ch_name=course_ch_name,
+            unit_num=unit_num,
+        )
+        course.save()
+
+        return CreateCourse(course=course)
+
+
+class ModifyLabel(graphene.Mutation):
+    label = graphene.Field(LabelType)
+
+    class Arguments:
+        label_name = graphene.String()
+        parent_id = graphene.Int()
+        label_level = graphene.Int()
+        skill_type = graphene.String()
+        label_type = graphene.String()
+        id = graphene.ID()
+
+    def mutate(self, info,
+               id, label_name,
+               label_level=None, skill_type=None, label_type=None, parent_id=None):
+        user = get_user(info) or None
+        if not user.is_active:
+            raise Exception('User: %s(%s) not active!' % (user.username, user.email))
+
+        label = Label.objects.get(pk=id)
+        label.label_name = label_name
+        label.save()
+
+        return ModifyLabel(label=label)
+
+
+class DeleteLabel(graphene.Mutation):
+    status = graphene.String()
+
+    class Arguments:
+        id = graphene.ID()
+
+    def mutate(self, info, id):
+        user = get_user(info) or None
+        if not user.is_active:
+            raise Exception('User: %s(%s) not active!' % (user.username, user.email))
+
+        label = Label.objects.get(pk=id)
+        label.delete()
+        return DeleteLabel(status="OK")
+
+
+
 class Mutation(graphene.ObjectType):
-    create_label= CreateLabel.Field()
+    create_label = CreateLabel.Field()
+    modify_label = ModifyLabel.Field()
+    delete_label = DeleteLabel.Field()
+    create_course = CreateCourse.Field()
