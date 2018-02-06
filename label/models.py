014f30554aa7a993ce0b3ec4161d9bb330302db6
pyskell <wangxulin108@gmail.com>
2018-02-06 16:24:56 +0800

    fix <Model, table_name> pair

--- label/models.py
+++ label/models.py
@@ -8,7 +8,7 @@ class Label(models.Model):
         ('XZ', '写作'),
         ('KY', '口语'),
     )
-    skill_type = models.CharField(max_length=20, verbose_name='技能分类', choices=SKILL_TYPE_CHOICES)
+    skill_type = models.CharField(max_length=20, verbose_name='技能分类', choices=SKILL_TYPE_CHOICES, default="TL")
 
     LABEL_TYPE_CHOICES = (
         ('WJN', '微技能'),
@@ -16,9 +16,9 @@ class Label(models.Model):
     )
     label_type = models.CharField(max_length=10, verbose_name='标签分类', choices=LABEL_TYPE_CHOICES)
 
-    label_level = models.IntegerField(verbose_name='标签级别')
+    level = models.IntegerField(db_column='label_level', verbose_name='标签级别', default=1)
 
-    label_name = models.CharField(max_length=50, verbose_name='名称')
+    name = models.CharField(db_column='label_name', max_length=50, verbose_name='名称', default="")
 
     parent = models.ForeignKey(
         'label.Label',
@@ -30,9 +30,15 @@ class Label(models.Model):
     date_joined= models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        db_table = 'label_dict'
+
 
 class Course(models.Model):
     id = models.CharField(db_column='course_id', max_length=100, verbose_name='课程ID', primary_key=True, default="")
     name = models.CharField(db_column='course_name', max_length=100, verbose_name='课程名称', default="")
     name_zh = models.CharField(db_column='course_ch_name', max_length=100, verbose_name='课程中文名称', default="")
     unit_num = models.IntegerField(verbose_name='单元数量', default=10)
+
+    class Meta:
+        db_table = 'course_info'
