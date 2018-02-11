577359bf7b33f9be6d6b1ae2a85393497d0c79b6
pyskell <wangxulin108@gmail.com>
2018-02-11 11:52:47 +0800

    fix `es` empty result error

--- material/gql.py
+++ material/gql.py
@@ -3,25 +3,20 @@ import utils
 import requests
 
 
+class Course(graphene.ObjectType):
+    course_id = graphene.String()
+    course_name = graphene.String()
+    course_ch_name = graphene.String()
+    unit_num = graphene.Int()
+
+
 class Label(graphene.ObjectType):
     id = graphene.Int()
     label_type = graphene.String()
-    label_level = graphene.String()
+    label_level = graphene.Int()
     label_name = graphene.String()
     parent_id = graphene.Int()
     skill_type = graphene.String()
-    # sub_labels = graphene.List(Label)
-
-    # def resolve_sub_labels(self, info):
-    #     with utils.get_conn().cursor() as cur:
-    #         cur.execute('''
-    #             select *
-    #             from {table_name}
-    #             where parent_id = %(parent_id)s
-    #             '''.format(**dict(table_name='label_dict')), dict(parent_id=self.id))
-    #         rows = cur.fetchall()
-    #         dicts = [dict(zip([c.name for c in cur.description], row)) for row in rows]
-    #         return [Question(**d) for d in dicts]
 
 
 class Discourse(graphene.ObjectType):
@@ -156,35 +151,11 @@ class Query(graphene.ObjectType):
         Title,
         title_ident=graphene.String()
     )
-    titles = graphene.List(
-        Title,
-        title_ident__in=graphene.List(graphene.String),
-        platform=graphene.String(), #平台
-        title_type=graphene.String(), #类型
-        title_course=graphene.String(), #教材
-        unit_id=graphene.String(), #单元
-        title_category=graphene.String(), #语言技能
-        title_info=graphene.String(), #语篇关键字
-        tag_status=graphene.String(), #语篇标注状态
-        review_status=graphene.String(), #语篇评审状态
-        label_tag_status=graphene.String(), #微技能标注状态
-        label_review_status=graphene.String(), #微技能评审状态
-        label_tag_user=graphene.String(), #微技能标注人
-        label_review_user=graphene.String(), #微技能评审人
-        discourse_tag_user=graphene.String(), #语篇标注人
-        discourse_review_user=graphene.String(), #语篇评审人
-        skill_level_1=graphene.String(), #一级微技能
-        skill_level_2=graphene.String(), #二级微技能
-        content_level_1=graphene.String(), #一级内容标签
-        content_level_2=graphene.String(), #二级内容标签
-        page_num=graphene.String(), #分页数
-        page_size=graphene.String(), #每页显示数量
-    )
     gp_labels = graphene.List(
         Label,
         id = graphene.Int(),
         label_type = graphene.String(),
-        label_level = graphene.String(),
+        label_level = graphene.Int(),
         label_name = graphene.String(),
         parent_id = graphene.Int(),
     )
@@ -212,16 +183,22 @@ class Query(graphene.ObjectType):
         page_num=graphene.String(), #分页数
         page_size=graphene.String(), #每页显示数量
     )
+    gp_courses = graphene.List(Course,)
 
     def resolve_gp_labels(self, info, **kwargs):
+        table_name = 'label_dict'
         with utils.get_conn().cursor() as cur:
-            cur.execute(
-                ''' select *
-                from {table_name}
-                where title_ident = %(title_ident)s
-                '''.format(**dict(table_name='label_dict')),
-                kwargs
-            )
+            if kwargs:
+                cur.execute(
+                    ''' select * from {table_name} where {conditions} '''.format(**dict(
+                        table_name=table_name,
+                        conditions='\n and '.join(
+                            map(lambda x: '{0} = %({0})s'.format(x), kwargs.keys())
+                        ))),
+                    kwargs
+                )
+            else:
+                cur.execute('select * from {table_name}'.format(**dict(table_name=table_name)))
             rows = cur.fetchall()
             dicts = [dict(zip([c.name for c in cur.description], row)) for row in rows]
             return [Label(**d) for d in dicts]
@@ -245,6 +222,14 @@ class Query(graphene.ObjectType):
         resp = requests.post(url, data=kwargs).json()
         es_rows = resp['result']
         with utils.get_conn().cursor() as cur:
+            if not es_rows:
+                return TitleList(
+                    total_num=resp['total_num'],
+                    page_size=kwargs['page_size'],
+                    page_num=kwargs['page_num'],
+                    titles=[]
+                )
+
             cur.execute(
                 '''
                 select *
@@ -262,6 +247,13 @@ class Query(graphene.ObjectType):
                 titles=[Title(**d) for d in dicts]
             )
 
+    def resolve_gp_courses(self, info, **kwargs):
+        with utils.get_conn().cursor() as cur:
+            cur.execute('select * from {table_name}'.format(**dict(table_name='course_info')),)
+            rows = cur.fetchall()
+            dicts = [dict(zip([c.name for c in cur.description], row)) for row in rows]
+            return [Course(**d) for d in dicts]
+
 
 class ChangeLabel(graphene.Mutation):
     label = graphene.Field(Label)
@@ -269,67 +261,65 @@ class ChangeLabel(graphene.Mutation):
     class Arguments:
         id = graphene.Int()
         label_type = graphene.String()
-        label_level = graphene.String()
+        label_level = graphene.Int()
         label_name = graphene.String()
         parent_id = graphene.Int()
         skill_type = graphene.String()
 
     def mutate(self, info, **kwargs):
-        with utils.get_conn().cursor() as cur:
-            columns = kwargs.keys()
-            label_id = kwargs.get('id')
-            if not label_id:
-                cur.execute(
-                    '''
-                    insert into {table_name} ({columns})
-                    values ({values})
-                    '''.format(
-                        **dict(
-                            table_name='label_dict',
-                            columns=', '.join(map(str, columns)),
-                            values=', '.join(map(lambda x: "%%(%s)s" % x, columns)),
-                        )
-                    ),
-                    kwargs
-                )
-            else:
-                cur.execute(
-                    '''
-                    update {table_name}
-                    set {columns}
-                    where id=%(id)s
-                    '''.format(**dict(
-                        table_name='label_dict',
-                        columns=',\n'.join(
-                            map(
-                                lambda x: '{0} = %({0})s'.format(x),
-                                columns
-                            )
-                        )
-                    )),
-                    kwargs
-                )
-            cur.execute(
-                ''' select * from {table_name} where {conditions} '''.format(**dict(
-                    table_name='label_dict',
-                    conditions='\n and '.join(
-                        map(lambda x: '{0} = %({0})s'.format(x), kwargs.keys())
-                    ))),
-                kwargs
-            )
-            row = cur.fetchone()
-            return Label(**dict(zip([c.name for c in cur.description], row)))
+        table_name = 'label_dict'
+        print(self)
+        return _mutate(ChangeLabel, "label", table_name, Label, kwargs, info)
+
+
+class DeleteLabel(graphene.Mutation):
+    status = graphene.String()
+
+    class Arguments:
+        id = graphene.Int()
+        label_type = graphene.String()
+        label_level = graphene.Int()
+        label_name = graphene.String()
+        parent_id = graphene.Int()
+        skill_type = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        table_name = 'label_dict'
+        return delete_mutate(DeleteLabel, table_name, Label, kwargs, info)
 
 
 class ChangeTitle(graphene.Mutation):
     title = graphene.Field(Title)
 
     class Arguments:
+        id = graphene.Int()
+        platform = graphene.String()
         title_ident = graphene.String()
+        title_detail_path = graphene.String()
+        title_type = graphene.String()
+        title_course = graphene.String()
+        title_category = graphene.String()
+        title_info = graphene.String()
+        title_url = graphene.String()
+        video_path = graphene.String()
+        version = graphene.String()
+        discourse_code = graphene.String()
+        block_id = graphene.String()
+        label_tag_user = graphene.String()
+        label_tag_status = graphene.String()
+        label_review_user = graphene.String()
+        label_review_status = graphene.String()
+        review_fail_reason = graphene.String()
+        tutorial_id = graphene.String()
+        tutorial_name = graphene.String()
+        unit_id = graphene.String()
+        unit_name = graphene.String()
+        minicoz_id = graphene.String()
+        minicoz_name = graphene.String()
 
     def mutate(self, info, title_ident, **kwargs):
-        import pdb
-        pdb.set_trace()
+        table_name = 'title_bank'
+        return _mutate(ChangeTitle, "title", table_name, Title, kwargs, info)
 
 
 class ChangeQuestion(graphene.Mutation):
@@ -345,50 +335,25 @@ class ChangeQuestion(graphene.Mutation):
         skill_level_2 = graphene.String()
 
     def mutate(self, info, **kwargs):
-        with utils.get_conn().cursor() as cur:
-            columns = kwargs.keys()
-            label_id = kwargs.get('id')
-            if not label_id:
-                cur.execute(
-                    '''
-                    insert into {table_name} ({columns})
-                    values ({values})
-                    '''.format(
-                        **dict(
-                            table_name='question_detail',
-                            columns=', '.join(map(str, columns)),
-                            values=', '.join(map(lambda x: "%%(%s)s" % x, columns)),
-                        )
-                    ),
-                    kwargs
-                )
-            else:
-                cur.execute(
-                    '''
-                    update {table_name}
-                    set {columns}
-                    where id=%(id)s
-                    '''.format(**dict(
-                        table_name='question_detail',
-                        columns=',\n'.join(
-                            map(
-                                lambda x: '{0} = %({0})s'.format(x),
-                                columns
-                            )
-                        )
-                    )),
-                    kwargs
-                )
-            cur.execute(
-                ''' select * from {table_name} where {conditions} '''.format(**dict(
-                    table_name='question_detail',
-                    conditions='\n and '.join(
-                        map(lambda x: '{0} = %({0})s'.format(x), kwargs.keys())
-                    ))),
-                kwargs
-            )
-            row = cur.fetchone()
-            return Label(**dict(zip([c.name for c in cur.description], row)))
+        table_name = 'question_detail'
+        return _mutate(ChangeQuestion, "question", table_name, Question, kwargs, info)
+
+
+class DeleteQuestion(graphene.Mutation):
+    status = graphene.String()
+
+    class Arguments:
+        id = graphene.Int()
+        question_index = graphene.Int()
+        title_ident = graphene.String()
+        content_level_1 = graphene.String()
+        content_level_2 = graphene.String()
+        skill_level_1 = graphene.String()
+        skill_level_2 = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        table_name = 'question_detail'
+        return delete_mutate(DeleteQuestion, table_name, Question, kwargs, info)
 
 
 class ChangeDiscourse(graphene.Mutation):
@@ -436,11 +401,12 @@ class ChangeDiscourse(graphene.Mutation):
 
     def mutate(self, info, **kwargs):
         table_name = 'discourse_quata'
-        return _mutate(table_name, Discourse, kwargs, info)
+        return _mutate(ChangeDiscourse, 'discourse', table_name, Discourse, kwargs, info)
 
 
-def _mutate(table_name, Class, kwargs, info):
-    with utils.get_conn().cursor() as cur:
+def _mutate(MutationClass, res_field_name, table_name, Class, kwargs, info):
+    conn = utils.get_conn()
+    with conn.cursor() as cur:
         columns = kwargs.keys()
         record_id = kwargs.get('id')
         if not record_id:
@@ -474,6 +440,7 @@ def _mutate(table_name, Class, kwargs, info):
                 )),
                 kwargs
             )
+        conn.commit()
         cur.execute(
             ''' select * from {table_name} where {conditions} '''.format(**dict(
                 table_name=table_name,
@@ -483,11 +450,35 @@ def _mutate(table_name, Class, kwargs, info):
             kwargs
         )
         row = cur.fetchone()
-        return Class(**dict(zip([c.name for c in cur.description], row)))
+        return MutationClass(
+            **{
+                res_field_name: Class(**dict(zip([c.name for c in cur.description], row)))
+            }
+        )
 
+
+def delete_mutate(MutationClass, table_name, Class, kwargs, info):
+    conn = utils.get_conn()
+    with conn.cursor() as cur:
+        cur.execute(
+            '''
+            delete from {table_name}
+            where {conditions}
+            '''.format(
+                **dict(
+                    table_name=table_name,
+                    conditions='\n and '.join(
+                        map(lambda x: '{0} = %({0})s'.format(x), kwargs.keys())
+                    ))),
+            kwargs
+        )
+        conn.commit()
+        return MutationClass(status="ok")
 
 class Mutation(graphene.ObjectType):
     change_title = ChangeTitle.Field()
-    change_label= ChangeLabel.Field()
+    change_label = ChangeLabel.Field()
+    delete_label = DeleteLabel.Field()
     change_question = ChangeQuestion.Field()
+    delete_question = DeleteQuestion.Field()
     change_discourse = ChangeDiscourse.Field()
